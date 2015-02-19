59c08c207eff39a9ef17d386b9c68a66787b7663
tstrott <tommy.strott@gmail.com>
2015-02-18 22:23:17 -0500

    Final test files, all ran successfully

---
+++ Project_1/test/CreateGiantInput.sh
@@ -0,0 +1,1 @@
+base64 /dev/urandom | head -c 1024000000 > GiantInputFile.txt
---
+++ Project_1/test/CreateMediumInput.sh
@@ -0,0 +1,2 @@
+base64 /dev/urandom | head -c 1024000 > MediumInputFileClient2.txt
+base64 /dev/urandom | head -c 1024000 > MediumInputFileClient3.txt
