7da11587ae6f6899f9912443f1d32c1c2960997b
tstrott <tommy.strott@gmail.com>
2015-03-04 18:42:16 -0500

    Project 2 test suite

---
+++ Project_2/test/DeleteDB.sh
@@ -0,0 +1,4 @@
+#delete database
+#note that while a server is active, a database.dat file exists and the data is stored internally
+#therefore this should be run while the server is not running
+rm ../build/database.txt
