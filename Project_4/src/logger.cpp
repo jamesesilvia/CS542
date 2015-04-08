7e315b02db20435bb179e3d88f0715dd48d9db4f
James Silvia <jamesesilvia@gmail.com>
2015-04-07 21:21:07 -0400

    open/close file on each r/w, improved stability

--- Project_4/src/logger.cpp
+++ Project_4/src/logger.cpp
@@ -17,34 +17,58 @@ Logger::Logger(string _tablename) {
     tablename = _tablename;
     // transaction count
     transaction = 0;
-    // Log file for transactions
-    string fileName = _tablename + ".log";
-    log_file.open(fileName.c_str(), ios_base::app);
 }
 
 
 void Logger::start_commit() {
     stringstream out;
+    string fileName = tablename + ".log";
+    
+    // open log file with append
+    log_file.open(fileName.c_str(), ios_base::app);
+    
+    // write to log file, starting transaction
     out << "START " << transaction << endl;
     log_file << out.str();
+
+    // cleanup
     log_file.flush();
+    log_file.close();
 }
 
 
 void Logger::update_entry(int ID, string old_val, string new_val) {
     stringstream out;
+    string fileName = tablename + ".log";
+    
+    // open log file with append
+    log_file.open(fileName.c_str(), ios_base::app);
+    
+    // write to log file, updating entry
     out << transaction << " " << ID << " " << 
             " " << old_val << " " << new_val << endl;
     log_file << out.str();
+    
+    // cleanup
     log_file.flush();
+    log_file.close();
 }
 
 
 void Logger::end_commit() {
     stringstream out;
+    string fileName = tablename + ".log";
+    
+    // open up log file with append
+    log_file.open(fileName.c_str(), ios_base::app);
+    
+    // write to log file, ending transaction
     out << "END " << transaction << endl;
     log_file << out.str();
+
+    // cleanup
     log_file.flush();
+    log_file.close();
     // Increment transaction count
     transaction++;
 }
